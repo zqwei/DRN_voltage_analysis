8b969266f5493a23db02aca6e435fbb882114dc5
Ziqiang Wei <weiz@janelia.hhmi.org>
2019-01-29 16:14:04 -0500

    fix a bug in swim bout detection

--- Notebooks/swim_dat.py
+++ Notebooks/swim_dat.py
@@ -30,8 +30,11 @@ def valid_swim(row, sig_thres=0.5, ismean=True, isplot=True):
         return False
     frame_stimParams = np.load(swim_dir/'frame_stimParams.npy')
     frame_swim_tcourse = np.load(swim_dir/'frame_swim_tcourse_series.npy')
-    swim_starts = np.where(np.diff((frame_swim_tcourse[0,:]>0).astype('int'))==1)[0]
-    swim_ends   = np.where(np.diff((frame_swim_tcourse[0,:]>0).astype('int'))==-1)[0]
+    swimdata = np.load(swim_dir/"swimdata.npy")[()]
+    swim_starts = (np.where(swimdata['swimStartT']>0)[0]/20).astype('int')
+    swim_ends = (np.where(swimdata['swimEndT']>0)[0]/20).astype('int')
+    # swim_starts = np.where(np.diff((frame_swim_tcourse[0,:]>0).astype('int'))==1)[0]
+    # swim_ends   = np.where(np.diff((frame_swim_tcourse[0,:]>0).astype('int'))==-1)[0]
     swim_ends   = swim_ends[((swim_starts>50) & (swim_starts<(frame_swim_tcourse.shape[1]-250)))]
     swim_starts = swim_starts[((swim_starts>50) & (swim_starts<(frame_swim_tcourse.shape[1]-250)))]
     task_period = frame_stimParams[2,swim_starts]
