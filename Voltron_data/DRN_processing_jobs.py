16fb4c30978e83431bc31a07be42900117b295b9
Ziqiang Wei <weiz@janelia.hhmi.org>
2019-02-04 15:35:50 -0500

    add try and catch to control the failures in processing

--- Voltron_data/DRN_processing_jobs.py
+++ Voltron_data/DRN_processing_jobs.py
@@ -122,38 +122,42 @@ def registration(is_largefile=True):
             continue
         if not os.path.isfile(save_folder+'/imgDMotion.tif') and os.path.isfile(save_folder + '/motion_fix_.npy'):
             if not os.path.isfile(save_folder+'/proc_registr.tmp'):
-                Path(save_folder+'/proc_registr.tmp').touch()
-                print(f'process file {folder}/{fish}')
-                imgD_ = imread(save_folder+'/imgDNoMotion.tif').astype('float32')
-                fix_ = np.load(save_folder + '/motion_fix_.npy').astype('float32')
-                if is_largefile:
-                    len_D_ = len(imgD_)//2
-                    motion_correction(imgD_[:len_D_], fix_, save_folder, ext='0')
-                    get_process_memory();
-                    motion_correction(imgD_[len_D_:], fix_, save_folder, ext='1')
-                    get_process_memory();
-                    imgD_ = None
-                    fix_ = None
-                    clear_variables((imgD_, fix_))
-                    s_ = [np.load(save_folder+'/imgDMotion%d.npy'%(__)) for __ in range(2)]
-                    s_ = np.concatenate(s_, axis=0).astype('float32')
-                    imsave(save_folder+'/imgDMotion.tif', s_, compress=1)
-                    s_ = None
-                    clear_variables(s_)
-                    os.remove(save_folder+'/imgDMotion0.npy')
-                    os.remove(save_folder+'/imgDMotion1.npy')
-                else:
-                    motion_correction(imgD_, fix_, save_folder)
-                    get_process_memory();
-                    imgD_ = None
-                    fix_ = None
-                    clear_variables((imgD_, fix_))
-                    s_ = np.load(save_folder+'/imgDMotion.npy').astype('float32')
-                    imsave(save_folder+'/imgDMotion.tif', s_, compress=1)
-                    s_ = None
-                    clear_variables(s_)
-                    os.remove(save_folder+'/imgDMotion.npy')
-                Path(save_folder+'/finished_registr.tmp').touch()
+                try:
+                    Path(save_folder+'/proc_registr.tmp').touch()
+                    print(f'process file {folder}/{fish}')
+                    imgD_ = imread(save_folder+'/imgDNoMotion.tif').astype('float32')
+                    fix_ = np.load(save_folder + '/motion_fix_.npy').astype('float32')
+                    if is_largefile:
+                        len_D_ = len(imgD_)//2
+                        motion_correction(imgD_[:len_D_], fix_, save_folder, ext='0')
+                        get_process_memory();
+                        motion_correction(imgD_[len_D_:], fix_, save_folder, ext='1')
+                        get_process_memory();
+                        imgD_ = None
+                        fix_ = None
+                        clear_variables((imgD_, fix_))
+                        s_ = [np.load(save_folder+'/imgDMotion%d.npy'%(__)) for __ in range(2)]
+                        s_ = np.concatenate(s_, axis=0).astype('float32')
+                        imsave(save_folder+'/imgDMotion.tif', s_, compress=1)
+                        s_ = None
+                        clear_variables(s_)
+                        os.remove(save_folder+'/imgDMotion0.npy')
+                        os.remove(save_folder+'/imgDMotion1.npy')
+                    else:
+                        motion_correction(imgD_, fix_, save_folder)
+                        get_process_memory();
+                        imgD_ = None
+                        fix_ = None
+                        clear_variables((imgD_, fix_))
+                        s_ = np.load(save_folder+'/imgDMotion.npy').astype('float32')
+                        imsave(save_folder+'/imgDMotion.tif', s_, compress=1)
+                        s_ = None
+                        clear_variables(s_)
+                        os.remove(save_folder+'/imgDMotion.npy')
+                    Path(save_folder+'/finished_registr.tmp').touch()
+                except Exception as err:
+                    print(f'Registration failed on file {folder}/{fish}: {err}')
+                    os.remove(save_folder+'/proc_registr.tmp')
     return None
 
 
@@ -175,29 +179,33 @@ def video_detrend():
 
         if not os.path.isfile(save_folder+'/Y_d.tif') and not os.path.isfile(save_folder+'/proc_detrend.tmp'):
             if os.path.isfile(save_folder+'/finished_registr.tmp'):
-                Path(save_folder+'/proc_detrend.tmp').touch()
-                Y = imread(save_folder+'/imgDMotion.tif').astype('float32')
-                Y = Y.transpose([1,2,0])
-                n_split = min(Y.shape[0]//cpu_count(), 8)
-                if n_split <= 1:
-                    n_split = 2
-                Y_len = Y.shape[0]//2
-                detrend(Y[:Y_len, :, :], save_folder, n_split=n_split//2, ext='0')
-                detrend(Y[Y_len:, :, :], save_folder, n_split=n_split//2, ext='1')
-                Y = None
-                clear_variables(Y)
-                get_process_memory();
-                Y = []
-                Y.append(np.load(save_folder+'/Y_d0.npy').astype('float32'))
-                Y.append(np.load(save_folder+'/Y_d1.npy').astype('float32'))
-                Y = np.concatenate(Y, axis=0).astype('float32')
-                imsave(save_folder+'/Y_d.tif', Y, compress=1)
-                Y = None
-                clear_variables(Y)
-                get_process_memory();
-                os.remove(save_folder+'/Y_d0.npy')
-                os.remove(save_folder+'/Y_d1.npy')
-                Path(save_folder+'/finished_detrend.tmp').touch()
+                try:
+                    Path(save_folder+'/proc_detrend.tmp').touch()
+                    Y = imread(save_folder+'/imgDMotion.tif').astype('float32')
+                    Y = Y.transpose([1,2,0])
+                    n_split = min(Y.shape[0]//cpu_count(), 8)
+                    if n_split <= 1:
+                        n_split = 2
+                    Y_len = Y.shape[0]//2
+                    detrend(Y[:Y_len, :, :], save_folder, n_split=n_split//2, ext='0')
+                    detrend(Y[Y_len:, :, :], save_folder, n_split=n_split//2, ext='1')
+                    Y = None
+                    clear_variables(Y)
+                    get_process_memory();
+                    Y = []
+                    Y.append(np.load(save_folder+'/Y_d0.npy').astype('float32'))
+                    Y.append(np.load(save_folder+'/Y_d1.npy').astype('float32'))
+                    Y = np.concatenate(Y, axis=0).astype('float32')
+                    imsave(save_folder+'/Y_d.tif', Y, compress=1)
+                    Y = None
+                    clear_variables(Y)
+                    get_process_memory();
+                    os.remove(save_folder+'/Y_d0.npy')
+                    os.remove(save_folder+'/Y_d1.npy')
+                    Path(save_folder+'/finished_detrend.tmp').touch()
+                except Exception as err:
+                    print(f'Detrend failed on file {folder}/{fish}: {err}')
+                    os.remove(save_folder+'/proc_detrend.tmp')
     return None
 
 
@@ -219,31 +227,31 @@ def local_pca():
 
         if not os.path.isfile(save_folder+'/proc_local_denoise.tmp'):
             if os.path.isfile(save_folder+'/finished_detrend.tmp'):
-                Path(save_folder+'/proc_local_denoise.tmp').touch()
-
-                if os.path.isfile(f'{save_folder}/Y_d.npy'):
-                    Y_d = np.load(f'{save_folder}/Y_d.npy').astype('float32')
-                elif os.path.isfile(f'{save_folder}/Y_d.tif'):
-                    Y_d = imread(f'{save_folder}/Y_d.tif')
-
-                Y_d_ave = Y_d.mean(axis=-1, keepdims=True) # remove mean
-                Y_d_std = Y_d.std(axis=-1, keepdims=True) # normalization
-                Y_d = (Y_d - Y_d_ave)/Y_d_std
-                Y_d = Y_d.astype('float32')
-                np.savez_compressed(f'{save_folder}/Y_2dnorm', Y_d_ave=Y_d_ave, Y_d_std=Y_d_std)
-                Y_d_ave = None
-                Y_d_std = None
-                clear_variables((Y_d_ave, Y_d_std))
-                get_process_memory();
-
-                for n, Y_d_ in enumerate(np.array_split(Y_d, 10, axis=-1)):
-                    denose_2dsvd(Y_d_, save_folder, ext=f'{n}')
-
-                Y_d_ = None
-                Y_d = None
-                clear_variables(Y_d)
-                get_process_memory();
-                Path(save_folder+'/finished_local_denoise.tmp').touch()
+                try:
+                    Path(save_folder+'/proc_local_denoise.tmp').touch()
+                    if os.path.isfile(f'{save_folder}/Y_d.npy'):
+                        Y_d = np.load(f'{save_folder}/Y_d.npy').astype('float32')
+                    elif os.path.isfile(f'{save_folder}/Y_d.tif'):
+                        Y_d = imread(f'{save_folder}/Y_d.tif')
+                    Y_d_ave = Y_d.mean(axis=-1, keepdims=True) # remove mean
+                    Y_d_std = Y_d.std(axis=-1, keepdims=True) # normalization
+                    Y_d = (Y_d - Y_d_ave)/Y_d_std
+                    Y_d = Y_d.astype('float32')
+                    np.savez_compressed(f'{save_folder}/Y_2dnorm', Y_d_ave=Y_d_ave, Y_d_std=Y_d_std)
+                    Y_d_ave = None
+                    Y_d_std = None
+                    clear_variables((Y_d_ave, Y_d_std))
+                    get_process_memory();
+                    for n, Y_d_ in enumerate(np.array_split(Y_d, 10, axis=-1)):
+                        denose_2dsvd(Y_d_, save_folder, ext=f'{n}')
+                    Y_d_ = None
+                    Y_d = None
+                    clear_variables(Y_d)
+                    get_process_memory();
+                    Path(save_folder+'/finished_local_denoise.tmp').touch()
+                except Exception as err:
+                    print(f'Local pca failed on file {folder}/{fish}: {err}')
+                    os.remove(save_folder+'/proc_local_denoise.tmp')
     return None
 
 
@@ -255,7 +263,6 @@ def demix_middle_data():
     from fish_proc.demix import superpixel_analysis as sup
     from fish_proc.utils.snr import correlation_pnr
     import pickle
-
     sns.set(font_scale=2)
     sns.set_style("white")
     dat_xls_file = pd.read_csv(dat_csv, index_col=0)
@@ -267,15 +274,12 @@ def demix_middle_data():
         image_folder = f'/nrs/ahrens/Takashi/0{folder}/{fish}/'
         save_folder = dat_folder + f'{folder}/{fish}/Data'
         save_image_folder = dat_folder + f'{folder}/{fish}/Results'
-
         if not os.path.exists(save_image_folder):
             os.makedirs(save_image_folder)
         print('=====================================')
         print(save_folder)
-
         if os.path.isfile(save_folder+'/finished_demix.tmp'):
             continue
-
         if not os.path.isfile(save_folder+'/proc_demix.tmp'):
             Path(save_folder+'/proc_demix.tmp').touch()
             _ = np.load(f'{save_folder}/Y_2dnorm.npz')
@@ -296,21 +300,18 @@ def demix_middle_data():
                 if os.path.isfile(f'{save_folder}/Y_2dsvd{n_}.npy'):
                     os.remove(f'{save_folder}/Y_2dsvd{n_}.npy')
             get_process_memory();
-
             plt.figure(figsize=(8, 4))
             plt.imshow(Y_d_std[:, :, 0])
             plt.colorbar()
             plt.tight_layout()
             plt.savefig(f'{save_image_folder}/Detrend_std.png')
             plt.close()
-
             len_Y = Y_svd.shape[-1]
             Y_svd_ = Y_svd[:, :, len_Y//4:-len_Y//4]
             d1, d2, _ = Y_svd_.shape
             Y_svd = None
             clear_variables(Y_svd)
             get_process_memory();
-
             mov_ = Y_svd_*Y_d_std + np.random.normal(size=Y_svd_.shape)*0.7
             Y_svd_ = None
             clear_variables(Y_svd_)
@@ -321,7 +322,6 @@ def demix_middle_data():
             plt.tight_layout()
             plt.savefig(f'{save_image_folder}/Denoised_local_corr_noised.png')
             plt.close()
-
             pass_num = 4
             cut_off_point=np.percentile(Cn.ravel(), [99, 95, 90, 85])
             rlt_= sup.demix_whole_data(-mov_, cut_off_point, length_cut=[25,25,25,25],
@@ -330,16 +330,12 @@ def demix_middle_data():
                                        merge_overlap_thr=0.6, num_plane=1, patch_size=[100, 100], plot_en=False,
                                        TF=False, fudge_factor=1, text=False, bg=False, max_iter=60,
                                        max_iter_fin=100, update_after=4)
-
             with open(f'{save_folder}/period_Y_demix_rlt.pkl', 'wb') as f:
                 pickle.dump(rlt_, f)
-
             print('Result file saved?')
             print(os.path.isfile(f'{save_folder}/period_Y_demix_rlt.pkl'))
-
             with open(f'{save_folder}/period_Y_demix_rlt.pkl', 'rb') as f:
                 rlt_ = pickle.load(f)
-
             for n_pass in range(pass_num):
                 sup.pure_superpixel_single_plot(rlt_["superpixel_rlt"][n_pass]["connect_mat_1"],
                                                 rlt_["superpixel_rlt"][n_pass]["unique_pix"],
@@ -378,7 +374,11 @@ def demix_components(ext=''):
     dat_xls_file = pd.read_csv(dat_csv, index_col=0)
     dat_xls_file['folder'] = dat_xls_file['folder'].apply(lambda x: f'{x:0>8}')
     for index, row in dat_xls_file.iterrows():
-        demix_middle_data_with_mask(row, ext=ext)
+        try:
+            demix_middle_data_with_mask(row, ext=ext)
+        except Exception as err:
+            print(f'Demix failed on file {folder}/{fish}: {err}')
+            os.remove(save_folder+'/proc_demix{ext}.tmp')
     return None
 
 
