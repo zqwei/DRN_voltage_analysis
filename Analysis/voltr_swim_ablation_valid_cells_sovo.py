539ba760f5cc99a929aff58dcf694bc9d9517643
Ziqiang Wei <weiz@janelia.hhmi.org>
2019-06-05 16:48:42 -0400

    add data for population analysis

---
+++ Analysis/voltr_swim_ablation_valid_cells_sovo.py
@@ -0,0 +1,80 @@
+"""
+This file uses to combine the swim data with the voltron activity of cells in the same recordings.
+
+Non-spiking neuron is removed
+
+Created on 03/21/2019
+@author: Ziqiang Wei
+@email: weiz@janelia.hhmi.org
+"""
+
+import numpy as np
+import pandas as pd
+import os
+from utils import smooth, boxcarKernel
+from scipy.signal import medfilt
+from scipy.stats import sem, ranksums
+
+dir_folder = '/nrs/ahrens/Ziqiang/Takashi_DRN_project/ProcessedData/'
+vol_file = 'depreciated/analysis_sections_ablation_gain_update.csv'
+dat_xls_file = pd.read_csv(vol_file, index_col=0)
+dat_xls_file['folder'] = dat_xls_file['folder'].apply(lambda x: f'{x:0>8}')
+
+non_spike_thres = 100
+cell_shape_thres = 1.3
+plot_ = False
+
+def spk_shape(spk_list, dff):
+    spk_ = []
+    for t_ in spk_list:
+        if t_+30<len(dff):
+            spk_.append(dff[t_-30:t_+30])
+    spk_ = np.array(spk_)
+    return spk_.mean(axis=0)
+
+
+for _, row in dat_xls_file.iterrows():
+    folder = row['folder']
+    fish = row['fish']
+    if not 'before' in fish:
+        continue
+    fish = row['fish'][:-6]
+    
+    dat_dir = dir_folder+f'{folder}/{fish}before/Data/'
+    dff = np.load(dat_dir+'Voltr_spikes.npz')['voltrs']
+    spk = np.load(dat_dir+'Voltr_spikes.npz')['spk']
+    dff = dff - np.nanmedian(dff, axis=1, keepdims=True)
+    num_cell = spk.shape[0]
+    spk = np.r_['-1', np.zeros((num_cell, 600)), spk]
+    
+    dat_dir = dir_folder+f'{folder}/{fish}after/Data/'
+    dff_ = np.load(dat_dir+'Voltr_spikes.npz')['voltrs']
+    spk_ = np.load(dat_dir+'Voltr_spikes.npz')['spk']
+    dff_ = dff_ - np.nanmedian(dff_, axis=1, keepdims=True)
+    num_cell = spk_.shape[0]
+    spk_ = np.r_['-1', np.zeros((num_cell, 600)), spk_]
+    valid_cell = np.zeros(num_cell).astype('bool')
+    cell_shape = np.zeros(num_cell)
+    cell_shape[:] = np.nan
+    
+    for n_cell in range(num_cell):
+        if (spk[n_cell].sum()<non_spike_thres) or (spk_[n_cell].sum()<non_spike_thres):
+            continue
+        spk_shape_b = spk_shape(np.where(spk[n_cell])[0], dff[n_cell])
+        spk_shape_a = spk_shape(np.where(spk_[n_cell])[0], dff_[n_cell])        
+        var_ = min(spk_shape_b.std(), spk_shape_a.std())
+        err_ = np.sqrt(((spk_shape_a-spk_shape_b)**2).mean())
+        cell_shape[n_cell] = err_/var_
+        if cell_shape[n_cell] < cell_shape_thres:
+            valid_cell[n_cell] = True
+        if plot_:
+            plt.plot(spk_shape_b)
+            plt.plot(spk_shape_a)
+            var_ = min(spk_shape_b.std(), spk_shape_a.std())
+            err_ = np.sqrt(((spk_shape_a-spk_shape_b)**2).mean())
+            plt.title(err_/var_)
+            plt.show()
+    
+    np.savez(f'swim_voltr/{folder}_{fish}_swim_voltr_valid_cell', \
+            cell_shape=cell_shape, \
+            valid_cell=valid_cell)
---
+++ Analysis/voltr_swim_gain_pop.py
@@ -0,0 +1,109 @@
+"""
+This file uses to combine the swim data with the voltron activity of cells in the same recordings.
+
+Non-spiking neuron is removed
+
+Created on 03/21/2019
+@author: Ziqiang Wei
+@email: weiz@janelia.hhmi.org
+"""
+
+import numpy as np
+import pandas as pd
+import os
+from utils import smooth, boxcarKernel
+from scipy.signal import medfilt
+from scipy.stats import sem, ranksums
+
+dir_folder = '/nrs/ahrens/Ziqiang/Takashi_DRN_project/ProcessedData/'
+vol_file = 'depreciated/analysis_sections_gain.csv'
+dat_xls_file = pd.read_csv(vol_file, index_col=0)
+dat_xls_file['folder'] = dat_xls_file['folder'].apply(lambda x: f'{x:0>8}')
+
+t_pre = 100 # time window pre-swim
+t_post = 350 # time window post-swim
+t_len = t_pre+t_post
+t_sig = 300 # time used for significance test after swim
+non_spike_thres = 0
+k_spk = boxcarKernel(sigma=61)
+k_sub = 10
+
+for _, row in dat_xls_file.iterrows():
+    folder = row['folder']
+    fish = row['fish']
+    dat_dir = dir_folder+f'{folder}/{fish}/Data/'
+    dff = np.load(dat_dir+'Voltr_spikes.npz')['voltrs']
+    spk = np.load(dat_dir+'Voltr_spikes.npz')['spk']
+    dff = dff - np.nanmedian(dff, axis=1, keepdims=True)
+    num_cell = spk.shape[0]
+    spk = np.r_['-1', np.zeros((num_cell, 600)), spk]
+
+    _ = np.load(f'swim_power/{folder}_{fish}_swim_dat.npz')
+    swim_starts = _['swim_starts']
+    swim_ends = _['swim_ends']
+    r_swim = _['r_swim']
+    l_swim = _['l_swim']
+    visu = _['visu']
+    task_period = _['task_period']
+    swim_task_index = _['swim_task_index']
+    p_swim = np.sqrt(r_swim**2+l_swim**2)
+
+    n_task = task_period.max().astype('int')
+    swim_task_index_ = (swim_task_index-1)%n_task+1
+    swim_bout_index_task = np.zeros(len(swim_task_index_)).astype('int')
+    swim_bout_index_task[0] = 1
+    for n_bout in range(len(swim_task_index_)-1):
+        if swim_task_index_[n_bout] == swim_task_index_[n_bout-1]:
+            swim_bout_index_task[n_bout] = swim_bout_index_task[n_bout-1]+1
+        else:
+            swim_bout_index_task[n_bout] = 1
+    
+    num_cell = dff.shape[0]
+    
+    trial_valid_ = np.ones(len(swim_starts)).astype('bool')
+    for n, n_swim in enumerate(swim_starts[:-1]):        
+        # examine the swim with short inter-swim-interval
+        if swim_starts[n+1] - n_swim < t_sig:    
+            trial_valid_[n] = False
+    
+    sub_swim = []
+    spk_swim = []
+    sub_sig_swim = []
+    # remove low spike cells
+    for n_cell in range(num_cell):
+        if spk[n_cell].sum()<non_spike_thres:
+            continue
+        n_spk = smooth(spk[n_cell], k_spk)
+        n_dff = medfilt(dff[n_cell], kernel_size=k_sub*2+1)
+        
+        sub_list = np.empty((r_swim.shape[0], t_len))
+        sub_list[:] = np.nan
+        sub_list_ = np.zeros((r_swim.shape[0], t_len))
+        spk_list = np.empty((r_swim.shape[0], t_len))
+        spk_list[:] = np.nan
+        sub_sig = np.ones(t_sig)
+        
+        for n, n_swim in enumerate(swim_starts):
+            if (n_swim>t_pre) and (n_swim+t_post<len(n_dff)):
+                # get sub and spk for each swim
+                sub_list[n, :] = n_dff[n_swim-t_pre:n_swim+t_post] 
+                sub_list_[n, :] = sub_list[n, :] - sub_list[n, (t_pre-30):t_pre].mean()
+                spk_list[n, :] = n_spk[n_swim-t_pre:n_swim+t_post]
+            else:
+                trial_valid_[n] = False
+            
+        ave_low = sub_list_[(task_period==1) & trial_valid_, :]
+        ave_high = sub_list_[(task_period==2) & trial_valid_, :]
+
+        if (ave_low.shape[0]>10) and (ave_high.shape[0]>10):
+            for _ in range(t_sig):
+                __, sub_sig[_] = ranksums(ave_low[:, _+t_pre], ave_high[:, _+t_pre])
+        
+        sub_swim.append(sub_list)
+        spk_swim.append(spk_list)
+        sub_sig_swim.append(sub_sig)
+    
+    np.savez(f'swim_voltr/{folder}_{fish}_swim_voltr_pop_dat', \
+            sub_swim=np.array(sub_swim), \
+            spk_swim=np.array(spk_swim), \
+            sub_sig_swim=np.array(sub_sig_swim), trial_valid=trial_valid_)
