885654ad565347e7daaeef70cae38e9c1b5190ea
Ziqiang Wei <weiz@janelia.hhmi.org>
2019-07-21 21:44:17 -0400

    add analyses for kernel fit

---
+++ KernelFits/sub_act.py
@@ -0,0 +1,112 @@
+"""
+This file uses to compute timing of inhibition and excitaion from subthreshould activity/
+
+Created on 03/22/2019
+@author: Ziqiang Wei
+@email: weiz@janelia.hhmi.org
+"""
+
+from scipy.stats import mannwhitneyu as utest
+from scipy.stats import ranksums
+from scipy.stats import ttest_ind
+import numpy as np
+from matplotlib import pyplot as plt
+
+def sub_process(dff, k_size=51):
+    from scipy.signal import medfilt
+    subvolt = dff.copy()
+    for n, ndff in enumerate(dff):
+        subvolt[n, :] = medfilt(ndff, kernel_size=k_size)
+    return subvolt
+
+def sub_input(ave_, c='k', t_min = 0, t_max = 450, t_zero=100, isplot=False):
+    mean_ = ave_[:, t_min:t_max].mean(axis=0)
+#     mean_ = mean_ - mean_[:(100-t_min)].max()
+    max_ = mean_.max()
+    min_ = mean_.min()
+    std_ = ave_[:, t_min:t_max].std(axis=0)/np.sqrt(ave_.shape[0])
+    t_start = -30 # -60
+    t_end = 270 # 270
+    t_step = 10 # 30
+    t_vec = np.arange(t_start, t_end, t_step)/300+t_step/2/300
+    act_ = mean_[t_zero+t_start:t_zero+t_end].reshape(-1, t_step)
+
+    if isplot:
+        plt.plot(np.arange(t_max-t_min)/300-(t_zero-t_min)/300, mean_, f'-{c}', lw=2)
+        plt.plot(np.arange(t_max-t_min)/300-(t_zero-t_min)/300, mean_-std_, f'--{c}', lw=0.5)
+        plt.plot(np.arange(t_max-t_min)/300-(t_zero-t_min)/300, mean_+std_, f'--{c}', lw=0.5)
+        plt.plot(np.arange(-60, 270, 30)/300+15/300, act_.mean(axis=-1), f'o{c}', ms=15, alpha=0.7)
+
+    
+    act_pre_ = act_[0]
+    dist = act_.max()-act_.min()
+    exc_input = []
+    pre_input = 0
+    for nperiod in range(act_.shape[0]-1):
+        _, p = ttest_ind(act_pre_, act_[nperiod+1])
+        diff_ = (act_[nperiod+1].mean()-act_pre_.mean())/dist
+        if p > .05 or np.abs(diff_)<0.1:
+            act_pre_ = np.concatenate([act_pre_, act_[nperiod+1]])
+            exc_input.append(pre_input)
+        else:
+            act_pre_ = act_[nperiod+1]
+            pre_input = np.sign(diff_)
+            exc_input.append(pre_input)
+            
+
+    exc_input = np.array(exc_input)
+    exc_input[1] = 0
+    act_mean = act_.mean(axis=-1)
+    
+    # print(exc_input.shape)
+    # print(act_mean)
+    
+    ### inh
+    # on time
+    try:
+        inh_on_ind = np.where(exc_input==-1)[0][0]+1
+        inh_on_t = t_vec[inh_on_ind-1] + np.argmax(act_[inh_on_ind-1])/300
+    except:
+        inh_on_t = np.nan
+    # peak time
+    try:
+        inh_peak_ind = np.where((exc_input[1:]-exc_input[:-1])==2)[0][0]+1
+    except:
+        if exc_input[-1]==-1:
+            inh_peak_ind = -1
+    try:
+        inh_peak_t = t_vec[inh_peak_ind] + np.argmin(act_[inh_peak_ind])/300
+    except:
+        inh_peak_t = np.nan
+    try:
+        inh_ = act_mean[inh_peak_ind] - act_mean[inh_on_ind-1]
+        if inh_ > 0:
+            inh_ = act_mean[inh_peak_ind] - act_mean[:inh_peak_ind].max()
+    except:
+        inh_ = 0
+        
+    ### exc
+    # on time
+    try:
+        exc_on_ind = np.where(exc_input==1)[0][0]+1
+        exc_on_t = t_vec[exc_on_ind-1] + np.argmin(act_[exc_on_ind-1])/300
+    except:
+        exc_on_t = np.nan
+    try:
+        exc_peak_ind = np.where((exc_input[1:]-exc_input[:-1])==-2)[0][0]+1
+    except:
+        if exc_input[-1]==1:
+            exc_peak_ind = -1
+    # peak time
+    try:
+        exc_peak_t = t_vec[exc_peak_ind] + np.argmax(act_[exc_peak_ind])/300
+    except:
+        exc_peak_t = np.nan
+    try:
+        exc_ = act_mean[exc_peak_ind] - act_mean[exc_on_ind-1]
+        if exc_ < 0:
+            exc_ = act_mean[exc_peak_ind] - act_mean[:exc_peak_ind].min()
+    except:
+        exc_ = 0
+
+    return [inh_on_t, inh_peak_t, inh_], [exc_on_t, exc_peak_t, exc_],
---
+++ KernelFits/utils.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# def smooth(a, kernel):
+#     b=np.convolve(a,kernel,'same')/np.convolve(np.ones(a.shape),kernel,'same')
+#     return b
+
+def smooth(a, kernel):
+    return np.convolve(a, kernel, 'full')[kernel.shape[0]//2:-(kernel.shape[0]//2)]
+
+
+def test_smooth(kernel):
+    x = np.zeros(1001)
+    x[30] = 1
+    y = smooth(x, kernel)
+    assert np.argmax(y)==30 # this is not working for boxcar kernel
+
+
+def boxcarKernel(sigma=60):
+    kernel = np.ones(sigma)
+    return kernel/kernel.sum()
+
+
+def gaussKernel(sigma=20):
+    kernel = (1/(np.sqrt(2*np.pi)*sigma))*np.exp(-(np.arange(-sigma*3,sigma*3+1)**2)/(2*sigma**2))
+    return kernel/kernel.sum()
+
+
+def bootstrap_p_ABtest(test, ctrl, is_left=True):
+    import bootstrapped.bootstrap as bs
+    import bootstrapped.stats_functions as bs_stats
+    import bootstrapped.compare_functions as bs_comp
+    num_ = 1000 # use for Multi-test correction
+    p_val = 0.05
+    boot_strp_iter = max(int(num_/p_val)*10+10, 10000)
+    num_thread = 30
+    test_ctrl_dist = bs.bootstrap_ab(test, ctrl, bs_stats.mean, bs_comp.difference, return_distribution=True, num_threads=num_thread, num_iterations=boot_strp_iter)
+    if is_left:
+        return (test_ctrl_dist<0).mean()
+    else:
+        return (test_ctrl_dist>0).mean()
+
+
+def cont_mode(data, isplot=False):
+    from scipy import stats
+    kde = stats.gaussian_kde(data)
+    x = np.linspace(data.min(), data.max(), 100)
+    p = kde(x)
+    if isplot:
+        plt.plot(x, p)
+    return x[np.argmax(p)]
+
+
+def plt_raster(spk_list, c='k', f_=300, t_shift=100, mz=10):
+    for n, ntrial in enumerate(spk_list):
+        t_ = np.where(ntrial==1)[0]-t_shift
+        plt.plot(t_/f_, np.ones(len(t_))*n, f'.{c}', markersize=mz)
+        
+
+def shaded_errorbar(x, y, error, ax=plt, color='k'):
+    ax.plot(x, y, '-', lw=1, color=color)
+    ax.fill_between(x, y-error, y+error, facecolor=color, lw=0, alpha=0.8)
