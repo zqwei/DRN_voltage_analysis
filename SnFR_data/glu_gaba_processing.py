5894151280a5a03231f7543731285f54af6476d9
Ziqiang Wei <weiz@janelia.hhmi.org>
2019-07-25 14:30:25 -0400

    update pipeline code

--- SnFR_data/glu_gaba_processing.py
+++ SnFR_data/glu_gaba_processing.py
@@ -4,10 +4,11 @@ import numpy as np
 import pandas as pd
 import os
 from pathlib import Path
-from nmf_calcium import *
+from preprocess_calcium import *
+# from nmf_calcium import *
 from utils_swim import *
 
-vol_file = '../Voltron_data/SnFR_Log_DRN_Exp.csv'
+vol_file = '../SnFR_data/SnFR_Log_DRN_Exp.csv'
 dat_xls_file = pd.read_csv(vol_file)
 dat_xls_file['folder'] = dat_xls_file['folder'].apply(lambda x: f'{x:0>8}')
 dat_folder = '/nrs/ahrens/Ziqiang/Takashi_DRN_project/SnFRData/'
@@ -23,31 +24,28 @@ for index, row in dat_xls_file.iterrows():
     dff_dir = dat_folder+f'{folder}/{fish}/Data/'
     swm_dir = dat_folder+f'{folder}/{fish}/swim/'
     img_dir = rootDir+f'{folder}/{fish}/Registered'
-    
     if not os.path.exists(swm_dir+'frame_swim_tcourse_series.npy'):
         swim(folder, fish, rootDir, dat_folder)
         trial_swim_power(folder, fish, dir_folder)
         frame_swim_power(folder, fish, dir_folder)
         frame_swim_power_series(folder, fish, dir_folder)
-    
-    
-    ave = np.load(img_dir+'/stack_sub.npy')[()]
-    np.save(dff_dir+'ave_img.npy', ave.mean(axis=0))
-    
-    if not os.path.exists(dff_dir):
-        os.makedirs(dff_dir)
-    if os.path.exists(dff_dir+'components.npz'):
-        continue
-    if os.path.exists(dff_dir+'processing.tmp'):
-        continue
-    
-    open(dff_dir+'processing.tmp', 'a').close()
-    print(dff_dir)
-    dFF = np.load(img_dir+'/dFF_sub.npy')[()]
-    dFF = dFF.transpose((1, 2, 0))
-    dFF = denoise_sig(dFF)
-    demix_components(dFF, dff_dir)
-    
-    os.remove(dff_dir+'processing.tmp')
-    
-    
+    pixel_denoise(row)
+    registration(row)
+    video_detrend(row)
+    local_pca_demix(row)
+
+    # if not os.path.exists(dff_dir):
+    #     os.makedirs(dff_dir)
+    # if os.path.exists(dff_dir+'components.npz'):
+    #     continue
+    # if os.path.exists(dff_dir+'processing.tmp'):
+    #     continue
+    #
+    # open(dff_dir+'processing.tmp', 'a').close()
+    # print(dff_dir)
+    # dFF = np.load(img_dir+'/dFF_sub.npy')[()]
+    # dFF = dFF.transpose((1, 2, 0))
+    # dFF = denoise_sig(dFF)
+    # demix_components(dFF, dff_dir)
+    #
+    # os.remove(dff_dir+'processing.tmp')
